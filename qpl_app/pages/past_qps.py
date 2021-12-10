138ca358389f96ab55cd32a57cfecb7a0d7357c7
ronishaiz <zahavi.roni@gmail.com>
2021-12-10 09:16:46 +0200

    Removed data to keep it in secrets

--- qpl_app/pages/past_qps.py
+++ qpl_app/pages/past_qps.py
@@ -8,28 +8,12 @@ def app():
 
     st.header('QP #17')
 
-    read_all_text(17)
+    read_text_from_secrets('museum')
 
 
-def read_all_text(qp_num: int):
-
-    data_folder = os.path.join('qpl_app', 'data')
-    qp_folder = os.path.join(data_folder, 'past_qps', f'qp{qp_num}')
-    files = glob.glob(os.path.join(qp_folder, '*.txt'))
-
-    if len(files) == 0:
-        raise Exception(f'did not find any text files in {qp_folder} folder')
-
-    text_header_to_text = {}
-
-    for file in files:
-        with open(file, 'r', encoding='utf-8') as f:
-            text_lines = f.readlines()
-        text_header_to_text[os.path.basename(file).replace('.txt', '')] = text_lines
-
-    for key in text_header_to_text:
-        st.subheader(key)
-        text_lines = text_header_to_text[key]
-        for line in text_lines:
-            text = f'<p style="font-family:Arial; font-size: 16px;">{line}</p>'
-            st.markdown(text, unsafe_allow_html=True)
+def read_text_from_secrets(secret_name: str):
+    text: str = st.secrets[secret_name]
+    split_out = text.split('\n')
+    for line in split_out:
+        text = f'<p style="font-family:Arial; font-size: 16px;">{line}</p>'
+        st.markdown(text, unsafe_allow_html=True)
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import streamlit as st
 from qpl_app.multipage import MultiPage
 from qpl_app.pages import past_qps, writing_together
 
+import os
+
 app = MultiPage()
 
 st.title('Welcome to the QP Library!')
@@ -11,3 +13,4 @@ app.add_page('Past QPs', past_qps.app)
 app.add_page('Writing Together', writing_together.app)
 
 app.run()
+
