d45dc8e66a6bb31bb2ac40a0606887fb14f7a8c5
ronishaiz <zahavi.roni@gmail.com>
2021-12-11 16:30:54 +0200

    Added songs played to QPs 5, 6

--- qpl_app/pages/qp5.py
+++ qpl_app/pages/qp5.py
@@ -3,4 +3,7 @@ from qpl_app.pages.page_utils import qp_page_app
 
 def app():
 
-    qp_page_app(5)
+    qp_page_app(5, songs_played_daria=['videotape', 'הזאב והאיילה', 'תחת עורי', 'ברד', 'בגלל הלילה',
+                                       'sex on fire', 'עירומה', 'כעץ אני'],
+                songs_played_roni=['ברד', 'sailor man', 'בצורת', 'supergirl', '505', 'sex on fire',
+                                   'violet hill', 'tessellate'])
--- qpl_app/pages/qp6.py
+++ qpl_app/pages/qp6.py
@@ -3,4 +3,7 @@ from qpl_app.pages.page_utils import qp_page_app
 
 def app():
 
-    qp_page_app(6)
+    qp_page_app(6, songs_played_daria=["הזאב והאיילה", "כעץ אני", "עירומה", "תחת עורי", "the animals",
+                                       "בגלל הלילה", "ברד", "videotape"],
+                songs_played_roni=["when you were young", "505", "ברד", "sailor man", "בצורת",
+                                   "אף פעם", "עיניים זרות", "supergirl"])
