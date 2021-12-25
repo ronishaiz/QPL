b147aa996d82b5daf32be14bc279857230662786
Roni <zahavi.roni@gmail.com>
2021-12-25 15:24:54 +0200

    Added all_writings page to app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 from qpl_app.multipage import MultiPage
-from qpl_app.pages import qp5, qp6, qp7, qp8, qp9, qp10, qp11, qp12, qp13, qp14, qp15, qp16, qp17
+from qpl_app.pages import qp5, qp6, qp7, qp8, qp9, qp10, qp11, qp12, qp13, qp14, qp15, qp16, qp17, all_writings
 
 app = MultiPage()
 
@@ -15,5 +15,7 @@ if user_value == st.secrets['password']:
 
     for num, qp in all_qps:
         app.add_page(f'QP #{num}', qp.app)
+
+    app.add_page('All Writings', all_writings.app)
 
     app.run()
