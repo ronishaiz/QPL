be069c1c6b63f180b8198fadc5bad216bbc01987
Roni <zahavi.roni@gmail.com>
2022-01-10 17:20:29 +0200

    added qp18

---
+++ qpl_app/pages/qp18.py
@@ -0,0 +1,7 @@
+from qpl_app.pages.page_utils import qp_page_app
+
+
+def app():
+
+    qp_page_app(18, songs_played_daria=["happiest of all memorial days", "move on", "חתונה לבנה", "hey ma"],
+                songs_played_roni=["train tracks", "missing the mark", "nothing breaks like a heart", "make it holy"])
