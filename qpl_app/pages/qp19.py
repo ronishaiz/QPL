676e8c36929402e25e9bcbfdc5633b289100abae
Roni <zahavi.roni@gmail.com>
2022-04-08 20:45:23 +0300

    added qp19

---
+++ qpl_app/pages/qp19.py
@@ -0,0 +1,8 @@
+from qpl_app.pages.page_utils import qp_page_app
+
+
+def app():
+
+    qp_page_app(19, songs_played_daria=["הזאב והאיילה", "motion sickness", "saudade", "last chance", "make it holy"],
+                songs_played_roni=["violet hill", "תמיד לפני הגשם", "queen (king) of spain", "motion sickness",
+                                   "כל העולם"])
