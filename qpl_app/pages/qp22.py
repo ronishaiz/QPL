32aa8c80c9dc3f46ddeb92bd41054bbf754c95ae
ronishaiz <69716822+ronishaiz@users.noreply.github.com>
2023-11-18 18:34:26 +0200

    added qp22 and qp23

---
+++ qpl_app/pages/qp22.py
@@ -0,0 +1,9 @@
+from qpl_app.pages.page_utils import qp_page_app
+
+
+def app():
+
+    qp_page_app(22, songs_played_daria=["walk above the city", "ceilings", "labradors", "beach baby", "pictures",
+                                        "heavy"],
+                songs_played_roni=["when you were young", "wanna be yours", "דרך הבגדים", "losing you", "dissolve me",
+                                   "שמיים בגוף", "atlas hands"])
